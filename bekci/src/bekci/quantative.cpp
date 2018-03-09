20ff96224009f2ea32dcb9264f29fedec4eb4001
serabuyukgoz <sera.buyukgoz@postgrad.plymouth.ac.uk>
2018-03-09 15:35:32 +0300

    comment added

--- bekci/src/bekci/quantative.cpp
+++ bekci/src/bekci/quantative.cpp
@@ -10,51 +10,66 @@
 #include <control_msgs/FollowJointTrajectoryActionGoal.h>
 #include <actionlib_msgs/GoalID.h>
 #include <stdlib.h>
+
 using namespace std;
+
 bool fault = false;
 ros::Publisher* pub;
 map<string,float> given_limits;
 string current_goal;
 vector<float> vec_keep;
-void goalIDKeeper(const control_msgs::FollowJointTrajectoryActionGoal& msg) {
+
+
+void goalIDKeeper(const control_msgs::FollowJointTrajectoryActionGoal& msg)
+{
     current_goal=msg.goal_id.id;
 }
-void limiter(const bekci::JointVelocity & msg) {
+
+//this is the main node which block and also subscriber
+void limiter(const bekci::JointVelocity & msg)
+{
 
     string name("elbow_joint");
-    actionlib_msgs::GoalID cancel;
-    for(int i=0;i<6;i++) {
-        if(name.compare(msg.names[i])==0) {
+    actionlib_msgs::GoalID cancel; //that lib for canceling moveit command **sera
+    for(int i=0;i<6;i++) //because of 6 joints
+    {
+        if(name.compare(msg.names[i])==0)
+        {
             vec_keep.push_back(msg.velocities[i]);
         }
+        //MEssages print to see
         ROS_INFO_STREAM(msg.names[i]<<" limit "<<given_limits[msg.names[i]]<<" message "<<msg.velocities[i]);
-        if(abs(given_limits[msg.names[i]]) < abs(msg.velocities[i])) {
+
+        if(abs(given_limits[msg.names[i]]) < abs(msg.velocities[i]))
+        {
             ROS_INFO_STREAM("wow");
             fault = true;
-            cancel.id=current_goal;
+            cancel.id=current_goal; //that lib for canceling moveit command **sera
             pub->publish(cancel);
             break;
         }
-
     }
-
-
 }
 
-
-
-int main(int argc,char** argv) {
+int main(int argc,char** argv)
+{
     ros::init(argc, argv, "quantative_limiter");
     ros::NodeHandle nh;
+
     string line;
     ifstream inFile("~/bekci/src/bekci/limits.txt");
     string::size_type sz;
     string pack("bekci");
     string  a= ros::package::getPath(pack);
+
     ROS_INFO_STREAM(a);
-    if(inFile.is_open()) {
+    //read file
+    if(inFile.is_open())
+    {
         ROS_INFO_STREAM("1");
-        while( getline(inFile,line)) {
+
+        while( getline(inFile,line))
+        {
             ROS_INFO_STREAM(line);
             vector<string> temp;
             boost::split(temp, line, boost::is_any_of("="));
@@ -70,16 +85,21 @@ int main(int argc,char** argv) {
     ros::Subscriber sub_goal = nh.subscribe("/follow_joint_trajectory/goal",1000,&goalIDKeeper);
 
     pub= new ros::Publisher(nh.advertise<actionlib_msgs::GoalID>("/follow_joint_trajectory/cancel",10));
-    while(ros::ok()) {
 
+    while(ros::ok())
+    {
         ros::spinOnce();
         rate.sleep();
     }
+
+    //I believe that part for creating graph write through the file
     ofstream outFile("~/bekci/src/bekci/output.txt");
-    if(outFile.is_open()) {
+    if(outFile.is_open())
+    {
         string temp("");
         string vir(",");
-        for(int i=0;i<vec_keep.size();i++) {
+        for(int i=0;i<vec_keep.size();i++)
+        {
             temp = temp+to_string(vec_keep[i])+vir;
         }
         outFile<<temp;
